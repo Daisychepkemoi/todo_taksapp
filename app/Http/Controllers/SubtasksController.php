1f43ad386864eea429b947257e96d137f0ab7838
Daisy Chepkemoi <dchepkemoi408@gmail.com>
2019-08-29 08:55:55 +0300

    initial commit with auth and migrations created

---
+++ app/Http/Controllers/SubtasksController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\subtasks;
+use Illuminate\Http\Request;
+
+class SubtasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\subtasks  $subtasks
+     * @return \Illuminate\Http\Response
+     */
+    public function show(subtasks $subtasks)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\subtasks  $subtasks
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(subtasks $subtasks)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\subtasks  $subtasks
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, subtasks $subtasks)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\subtasks  $subtasks
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(subtasks $subtasks)
+    {
+        //
+    }
+}
